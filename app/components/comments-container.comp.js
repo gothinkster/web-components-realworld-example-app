b7f181618aadbcd6cf92ba7d86643dcce26c5088
codeprefect <mubarakadeimam@gmail.com>
2018-08-24 18:53:44 +0100

    fix unexpected token error in comments-container.comp.js

--- app/components/comments-container.comp.js
+++ app/components/comments-container.comp.js
@@ -1,40 +1,40 @@
 export class CommentsContainerComponent extends HTMLElement {
-    constructor() {
-        super();
-        this.comments = [];
-    }
+  constructor() {
+    super();
+    this.comments = [];
+  }
 
-    static get observedAttributes() {
-        return [];
-    }
+  static get observedAttributes() {
+    return [];
+  }
 
-    attributeChangedCallback(name, oldValue, newValue) {
+  attributeChangedCallback(name, oldValue, newValue) {
 
-    }
+  }
 
-    connectedCallback() {
-        this.slug = this.getAttribute('slug');
-        fetch('https://conduit.productionready.io/api/articles/' + this.slug + '/comments')
-            .then(response => response.json())
-            .then(r => {
-                this.comments = r.comments;
-                this.innerHTML = this.render();
-            });
-    }
-
-    disconnectedCallback() {
-
-    }
-
-    refresh() {
+  connectedCallback() {
+    this.slug = this.getAttribute('slug');
+    fetch('https://conduit.productionready.io/api/articles/' + this.slug + '/comments')
+      .then(response => response.json())
+      .then(r => {
+        this.comments = r.comments;
         this.innerHTML = this.render();
-    }
+      });
+  }
 
-    render() {
-        return `
-            ${this.comments.map(comment => `
-                <comment-preview 
-                    username="${comment.author.username}" 
+  disconnectedCallback() {
+
+  }
+
+  refresh() {
+    this.innerHTML = this.render();
+  }
+
+  render() {
+      return `
+            ${this.comments.map(comment => {`
+                <comment-preview
+                    username="${comment.author.username}"
                     content="${comment.body}"
                     image="${comment.author.image}"
                     created-at="${comment.createdAt}"
