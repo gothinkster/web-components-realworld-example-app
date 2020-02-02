5a45fad98e2aa9b41ee460f19f488a3800716384
Reinout Claeys <reinoutclaeys@gmail.com>
2020-02-02 22:57:56 +0100

    Update webpack.config.js
    var can be const

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
